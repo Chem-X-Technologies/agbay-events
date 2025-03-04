80e98a397d53535f521a1f4e76c4999d362c627f
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2025-03-04 21:15:24 +0800

    Enhanced attendee validation screen to check if ticket was already validated and update UI accordingly

--- app/attendees/validate/[id].tsx
+++ app/attendees/validate/[id].tsx
@@ -6,10 +6,11 @@ import LoadingSpinner from '~/components/shared/LoadingSpinner';
 import { CheckCircle2 } from '~/lib/icons/CheckCircle2';
 import { XCircle } from '~/lib/icons/XCircle';
 import { Button } from '~/components/ui/button';
-import { editAttendee } from '~/lib/services/attendeeService';
+import { editAttendee, getAttendeeById } from '~/lib/services/attendeeService';
 import Attendee, { AttendeeStatus } from '~/lib/types/attendee';
 import { Text } from '~/components/ui/text';
 import { Card, CardHeader, CardTitle } from '~/components/ui/card';
+import { Info } from '~/lib/icons/Info';
 
 export default function ValidateAttendeeScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
@@ -28,10 +29,8 @@ export default function ValidateAttendeeScreen() {
     },
   });
   const [isSuccess, setIsSuccess] = useState(false);
-
-  useEffect(() => {
-    mutation.mutate({ status: AttendeeStatus.Attended });
-  }, []);
+  const [alreadyAttended, setAlreadyAttended] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleViewEventPress = () => {
     router.replace({
@@ -40,32 +39,57 @@ export default function ValidateAttendeeScreen() {
     });
   };
 
+  useEffect(() => {
+    setLoading(true);
+    getAttendeeById(id)
+      .then((attendee) => {
+        if (!attendee) return;
+
+        if (attendee.status === AttendeeStatus.Attended) {
+          setAlreadyAttended(true);
+          setEventId(attendee.eventId);
+        } else {
+          mutation.mutate({ status: AttendeeStatus.Attended });
+        }
+      })
+      .catch(() => setIsSuccess(false))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading || mutation.isPending) return <LoadingSpinner />;
+
   return (
     <View className="flex-1 justify-center p-4 gap-10 bg-secondary">
-      {mutation.isPending ? (
-        <LoadingSpinner />
-      ) : (
-        <>
-          <Card className="w-full p-2 rounded-2xl">
-            <CardHeader className="items-center gap-4">
-              {isSuccess ? (
-                <CheckCircle2 size={60} className="text-green-400" />
-              ) : (
-                <XCircle size={60} className="text-red-400" />
-              )}
-              <CardTitle className="text-center">
-                {isSuccess
-                  ? 'Ticket validated successfully!'
-                  : 'Ticket validation failed. Kindly try again.'}
-              </CardTitle>
-            </CardHeader>
-          </Card>
+      <Card className="w-full p-2 rounded-2xl">
+        <CardHeader className="items-center gap-4">
+          {alreadyAttended ? (
+            <Info size={60} className="text-gray-400" />
+          ) : isSuccess ? (
+            <CheckCircle2 size={60} className="text-green-400" />
+          ) : (
+            <XCircle size={60} className="text-red-400" />
+          )}
+          <CardTitle
+            className={`text-center ${
+              alreadyAttended
+                ? 'text-gray-400'
+                : isSuccess
+                ? 'text-green-500'
+                : 'text-red-500'
+            }`}
+          >
+            {alreadyAttended
+              ? 'Ticket was already validated before'
+              : isSuccess
+              ? 'Ticket validated successfully!'
+              : 'Ticket validation failed. Kindly try again.'}
+          </CardTitle>
+        </CardHeader>
+      </Card>
 
-          <Button onPress={handleViewEventPress}>
-            <Text>View Event</Text>
-          </Button>
-        </>
-      )}
+      <Button onPress={handleViewEventPress}>
+        <Text>View Event</Text>
+      </Button>
     </View>
   );
 }

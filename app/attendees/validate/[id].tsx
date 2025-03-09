4e0bbb8a83bd462e9f09d6eab1edef524a2d0c4e
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2025-03-09 15:55:35 +0800

    Refactor ValidateAttendeeScreen to fetch attendee and event data together, enhance loading state management, and integrate TicketCard for displaying attendee information.

--- app/attendees/validate/[id].tsx
+++ app/attendees/validate/[id].tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Link, useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import LoadingSpinner from '~/components/shared/LoadingSpinner';
@@ -11,12 +11,28 @@ import Attendee, { AttendeeStatus } from '~/lib/types/attendee';
 import { Text } from '~/components/ui/text';
 import { Card, CardHeader, CardTitle } from '~/components/ui/card';
 import { Info } from '~/lib/icons/Info';
+import TicketCard from '~/components/screens/attendees/TicketCard';
+import { getEventById } from '~/lib/services/eventService';
+import AgbayEvent from '~/lib/types/agbay-event';
+import { ImageBackground } from 'expo-image';
+import { cn } from '~/lib/utils';
+
+const fetchData = async (attendeeId: string) => {
+  const attendee = await getAttendeeById(attendeeId);
+  const event = await getEventById(attendee?.eventId ?? '');
+  return { attendee, event };
+};
 
 export default function ValidateAttendeeScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
-  const [eventId, setEventId] = useState('');
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [alreadyAttended, setAlreadyAttended] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<{
+    attendee?: Attendee;
+    event?: AgbayEvent;
+  }>();
   const queryClient = useQueryClient();
-  const router = useRouter();
   const mutation = useMutation({
     mutationFn: (updatedData: Partial<Attendee>) =>
       editAttendee(id, updatedData),
@@ -25,41 +41,74 @@ export default function ValidateAttendeeScreen() {
         queryKey: [`events/${attendee?.eventId}`],
       });
       setIsSuccess(true);
-      setEventId(attendee?.eventId ?? '');
+    },
+    onError: (error) => {
+      console.error(`Failed to update ticket status: ${error.message}`);
+      setIsSuccess(false);
     },
   });
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [alreadyAttended, setAlreadyAttended] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const handleViewEventPress = () => {
-    router.replace({
-      pathname: '/events/details/[id]',
-      params: { id: eventId },
-    });
-  };
 
   useEffect(() => {
     setLoading(true);
-    getAttendeeById(id)
-      .then((attendee) => {
-        if (!attendee) return;
+    fetchData(id)
+      .then((data) => {
+        if (!data?.attendee) return;
 
-        if (attendee.status === AttendeeStatus.Attended) {
+        if (data.attendee.status === AttendeeStatus.Attended) {
           setAlreadyAttended(true);
-          setEventId(attendee.eventId);
         } else {
           mutation.mutate({ status: AttendeeStatus.Attended });
         }
+
+        setData(data);
       })
-      .catch(() => setIsSuccess(false))
+      .catch((err) => {
+        console.error(`Failed to fetch data: ${err.message}`);
+        setIsSuccess(false);
+      })
       .finally(() => setLoading(false));
   }, []);
 
   if (loading || mutation.isPending) return <LoadingSpinner />;
 
+  return !!data?.event?.poster?.url ? (
+    <ImageBackground
+      source={{ uri: data.event.poster.url }}
+      contentFit="cover"
+      style={{ flex: 1 }}
+    >
+      <ValidateAttendeeScreenContent
+        alreadyAttended={alreadyAttended}
+        isSuccess={isSuccess}
+        data={data}
+      />
+    </ImageBackground>
+  ) : (
+    <ValidateAttendeeScreenContent
+      alreadyAttended={alreadyAttended}
+      isSuccess={isSuccess}
+      data={data}
+      className="bg-secondary"
+    />
+  );
+}
+
+const ValidateAttendeeScreenContent = ({
+  alreadyAttended,
+  isSuccess,
+  data,
+  className,
+}: {
+  alreadyAttended: boolean;
+  isSuccess: boolean;
+  data?: {
+    attendee?: Attendee;
+    event?: AgbayEvent;
+  };
+  className?: string;
+}) => {
   return (
-    <View className="flex-1 justify-center p-4 gap-10 bg-secondary">
+    <View className={cn('flex-1 p-4 gap-4', className)}>
       <Card className="w-full p-2 rounded-2xl">
         <CardHeader className="items-center gap-4">
           {alreadyAttended ? (
@@ -86,10 +135,22 @@ export default function ValidateAttendeeScreen() {
           </CardTitle>
         </CardHeader>
       </Card>
-
-      <Button onPress={handleViewEventPress}>
-        <Text>View Event</Text>
-      </Button>
+      {!!data?.attendee && !!data?.event && (
+        <TicketCard attendee={data.attendee} event={data.event} />
+      )}
+      <View className="flex-1 justify-end">
+        <Link
+          href={{
+            pathname: '/events/details/[id]',
+            params: { id: data?.event?.id ?? '' },
+          }}
+          asChild
+        >
+          <Button>
+            <Text>View Event</Text>
+          </Button>
+        </Link>
+      </View>
     </View>
   );
-}
+};
